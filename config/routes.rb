3f23746208a17c36013b7f38ed8927ff3c1a0be4
Ubuntu <ubuntu@ip-172-31-46-117.ap-northeast-1.compute.internal>
2021-03-08 00:32:37 +0000

    rotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,32 @@
 Rails.application.routes.draw do
-  
-  get "/reservations" => "reservations#index"
-  get "reservations/new" => "reservations#new"
-  post "reservations/create" => "reservations#create"
-  get "reservations/:id" => "reservations#show"
-  post "reservations/:id/destroy" => "reservations#destroy"
+
+resources :reservations  
+  # get "/reservations" => "reservations#index"
+  # get "reservations/new" => "reservations#new"
+  # post "reservations/create" => "reservations#create"
+  # get "reservations/:id" => "reservations#show"
+  # post "reservations/:id/destroy" => "reservations#destroy"
 
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
   post "users/:id/destroy" => "users#destroy"
-  
-  get "sign_up" => "users#new" 
-  get "users/:id/edit" => "users#edit"
-  post "users/:id/update" => "users#update"
-  post "users/create" => "users#create"
-  get "users/:id" => "users#show"
+
+  resources :users
+  # get "sign_up" => "users#new" 
+  # get "users/:id/edit" => "users#edit"
+  # post "users/:id/update" => "users#update"
+  # post "users/create" => "users#create"
+  # get "users/:id" => "users#show"
  
   get "search" => "rooms#search"
-  get "rooms/new" => "rooms#new"
-  post "rooms/create" => "rooms#create"
-  get "rooms/:id" => "rooms#show"
-  get "rooms/:id/edit" => "rooms#edit"
-  post "rooms/:id/update" => "rooms#update"
-  post "rooms/:id/destroy" => "rooms#destroy"
+  resources :rooms
+  # get "rooms/new" => "rooms#new"
+  # post "rooms/create" => "rooms#create"
+  # get "rooms/:id" => "rooms#show"
+  # get "rooms/:id/edit" => "rooms#edit"
+  # post "rooms/:id/update" => "rooms#update"
+  # post "rooms/:id/destroy" => "rooms#destroy"
   
   get "/" => "home#top"
   
